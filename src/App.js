8f239d8bd05d72d862fe50a99b63a38e613e2f75
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-13 12:28:48 +0100

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import AppNavbar from './components/Navbar';
 
 class App extends Component {
   render() {
+    const includedNavs = [
+      {name: 'DASHBOARD'},
+      {name: 'ACCOUNT'}
+    ];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <AppNavbar includedNavs={includedNavs} signedIn={true}/>
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+class AppNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  getMenuItems() {
+    return this.props.includedNavs.map( (navDescription, index) => {
+      const props = this.props;
+      switch(navDescription.name){
+        case 'DASHBOARD':
+          return (
+            <NavItem key={index}>
+              <NavLink href="/dashboard/">Dashboard</NavLink>
+            </NavItem>
+          );
+        case 'ACCOUNT':
+          return (
+            <UncontrolledDropdown nav inNavbar key={index}>
+              <DropdownToggle nav caret>
+                Account
+              </DropdownToggle>
+              <DropdownMenu >
+                { function() {
+                  if(props.signedIn){
+                    return (
+                      <DropdownItem>
+                        Sign Out
+                      </DropdownItem>
+                    );
+                  }
+                  else {
+                    return (
+                      <DropdownItem>
+                        Sign In
+                      </DropdownItem>
+                    );
+                  }
+                }()}
+                <DropdownItem divider />
+                  { function() {
+                    if(props.signedIn){
+                      return (
+                        <DropdownItem>
+                          Edit Account
+                        </DropdownItem>
+                      );
+                    }
+                    else{
+                      return (
+                        <DropdownItem>
+                          Register Account
+                        </DropdownItem>
+                      );
+                    }
+                  }()}
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          );
+        default:
+          console.log('Unknown nav item name ' + navDescription.name);
+          return <span style={{margin: 0, padding: 0}} key={index}></span>
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">Miniminer</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              { this.getMenuItems() }
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
