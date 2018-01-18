bd855af55f0323f29a2e4b8cc10d540ab1ef235b
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-19 00:17:35 +0100

    Added redux store and routing support to the index js app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Route, Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import store, {history} from './store';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
 registerServiceWorker();
