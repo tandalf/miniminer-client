b540327378d96ad09818d8c45cab6473575f1530
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-23 10:35:06 +0100

    Added basic registration form and auth route

---
+++ src/components/pages/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import RegistrationForm from '../forms/Registration';
+
+const AuthPage = (props) => {
+  return (
+    <RegistrationForm />
+  );
+}
+
+export default AuthPage;
--- src/components/pages/PageContent.js
+++ src/components/pages/PageContent.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './Home';
 import DashboardPage from './Dashboard';
+import AuthPage from './Auth';
 
 class PageMainContent extends React.Component {
   render(){
@@ -11,6 +12,7 @@ class PageMainContent extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/dashboard" component={DashboardPage}></Route>
+          <Route path="/auth" component={AuthPage}></Route>
         </Switch>
       </div>
     );
