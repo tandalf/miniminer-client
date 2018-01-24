e109a272324b6df73b3376d25902f7bbeaa0beeb
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-24 20:15:50 +0100

    Added login form

--- src/components/pages/Auth.js
+++ src/components/pages/Auth.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import RegistrationForm from '../forms/Registration';
+import LoginForm from '../forms/Login';
 
 const AuthPage = (props) => {
   return (
-    <RegistrationForm />
+    <Switch>
+      <Route path="/auth/login" component={LoginForm}></Route>
+      <Route path="/auth/register" component={RegistrationForm}></Route>
+    </Switch>
+
   );
 }
 
