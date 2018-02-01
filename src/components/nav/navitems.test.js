c95789662702059df3b6083725dbf2020f20e393
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-02-01 10:31:40 +0100

    Tested that account menu item should contain 'Edit Account' submenu item

--- src/components/nav/navitems.test.js
+++ src/components/nav/navitems.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store'
+import { DropdownItem } from 'reactstrap';
 
 import { AccountMenuItem } from './navitems';
 
@@ -9,17 +10,26 @@ const middlewares = []
 const mockStore = configureStore(middlewares)
 
 describe('<AccountMenuItem />', () => {
+  const signedInUser = {signedIn: true};
   it('should contain sign out submenu item if user is signed in', () => {
-    const user = {
-      signedIn: true
-    };
     const initialState = {
-      user
-    }
+      user: signedInUser
+    };
     const store = mockStore(initialState);
-    const accountMenuItem = mount((<Provider store={store}><AccountMenuItem user={user} /></Provider>));
-    const signOutSubMenuItem = <button>Sign Out</button>;
+    const accountMenuItem = mount((<Provider store={store}><AccountMenuItem user={signedInUser} /></Provider>));
+    const signOutSubMenuItem = <DropdownItem>Sign Out</DropdownItem>;
 
     expect(accountMenuItem.containsMatchingElement(signOutSubMenuItem)).toEqual(true);
+  });
+
+  it("should contain an 'Edit Account' submenu item if the user is signedIn", () => {
+    const initialState = {
+      user: signedInUser
+    };
+    const store = mockStore(initialState);
+    const accountMenuItem = mount((<Provider store={store}><AccountMenuItem user={signedInUser} /></Provider>));
+    const editAccountSubMenuItem = <DropdownItem>Edit Account</DropdownItem>;
+
+    expect(accountMenuItem.containsMatchingElement(editAccountSubMenuItem)).toEqual(true);
   });
 });
